24e00813fef73d339b87af5158a6728e22ff93e9
thang280699 <43412000+thang280699@users.noreply.github.com>
2018-10-11 22:20:06 +0700

    Add files via upload

---
+++ src/week4/task1/Cam.java
@@ -0,0 +1,21 @@
+
+package week4;
+
+
+    public class Cam extends HoaQua {
+    public double giaban  ;
+    public int soluong    ;
+    public void setGiaban(double Giaban){
+        this.giaban = Giaban ;
+    }
+    public void setSoluong(int Soluong){
+        this.soluong = Soluong;
+    }
+    public double getGiaban(){
+        return this.giaban ;
+    }
+    public int getSoluong(){
+        return this.soluong ;
+    }
+    
+}
