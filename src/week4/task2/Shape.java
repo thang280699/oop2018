b6c364b3be2d0b86a8a842c7b424c89beb8c9f98
thang280699 <43412000+thang280699@users.noreply.github.com>
2018-10-12 16:54:19 +0700

    Add files via upload

---
+++ src/week4/task2/Shape.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4.tast2;
+
+/**
+ *
+ * @author CCNE
+ */
+public class Shape {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
