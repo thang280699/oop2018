1ed0f55e97fc8b89064f4e99f7ab601b944d0db2
thang280699 <43412000+thang280699@users.noreply.github.com>
2018-11-02 16:42:13 +0700

    Add files via upload

---
+++ src/week7/Expression.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7;
+
+/**
+ *
+ * @author CCNE
+ */
+abstract public class Expression {
+    @Override
+   abstract public String toString();
+    abstract public int evaluate();
+    
+}
