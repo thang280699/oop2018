d8106dfc0e9cac84a24cd736e6f8eb0f527a2ba2
thang280699 <43412000+thang280699@users.noreply.github.com>
2018-11-23 16:34:59 +0700

    Add files via upload

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,34 @@
+
 package week11;
 
+
 public class Task1 {
+    public static <T extends Comparable> void sx(T[] a){
+        T x;
+        int n= a.length;
+        for (int i = 0;i<n-1;i++) {
+            for(int y=0;y<n - i -1;y++){
+                if(a[y].compareTo(a[y+1])>0){
+                    x = a[y];
+                    a[y] = a[y+1];
+                    a[y+1] = x;
+                    
+                }
+            
+            }
+        }
+        for(int y=0;y<n;y++){
+            
+            System.out.println(a[y]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+   Byte[] a = { 5,6,7,8};
+   Task1 t = new Task1();
+   t.sx(a);
+   
+           
+    }
+    
 }
